d16014839dd1b33ae295621d6b955b2cf6314d29
K-KY <kyuyoungk@naver.com>
2025-12-09 19:41:44 +0900

    feat: 스토리지 디렉토리 요청 axios

---
+++ src/axios/StorageApi.ts
@@ -0,0 +1,14 @@
+import type {StorageRequest} from "../types/StorageRequest.ts";
+import axios from "axios";
+
+const getDirs = async (request: StorageRequest) => {
+    return await axios({
+        url:"http://localhost:8080/api/v1/dirs",
+        method: "POST",
+        data: request,
+    }).then((response) => {
+        return response.data;
+    })
+}
+
+export {getDirs}
