34507cc2499249c164f6e0a0ba9082aec1e460dd
K-KY <kyuyoungk@naver.com>
2025-12-11 20:27:03 +0900

    feat: 하위 폴더 이동 상위 폴더 이동으로 디렉토리 탐색 가능하게 변경

--- src/components/FileViewer.tsx
+++ src/components/FileViewer.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {getDirs, getFiles} from "../axios/StorageApi.ts";
+import {getDirs, getFiles, getParentDir} from "../axios/StorageApi.ts";
 import {BorderLayout} from "./BoarderLayout.tsx";
 import type {FileInfo} from "../types/FileInfo.ts";
 import type {DirectoryInfo} from "../types/DirectoryInfo.ts";
@@ -8,29 +8,53 @@ const FileViewer = () => {
     const [files, setFiles] = useState<FileInfo[]>([]);
     const [dirs, setDirs] = useState<DirectoryInfo[]>([]);
     //undefined인 경우 사용자의 루트 디렉토리 조회
-    const [currentParentSeq, setCurrentParentSeq] = useState<number>(2);
+    const [currentDirSeq, setCurrentDirSeq] = useState<number>(2);
 
     useEffect(() => {
+        console.log("use")
         getDirs({
             ownerId: "김규영",
-            parentSeq: currentParentSeq,
+            parentSeq: currentDirSeq,
         }).then(res => setDirs(res));
 
         getFiles({
             ownerId: "김규영",
-            parentSeq: currentParentSeq,
+            parentSeq: currentDirSeq,
         }).then(res => {
             setFiles(res)
         });
-    }, [currentParentSeq])
+    }, [currentDirSeq])
 
+    //디렉토리 눌렀을 때
+    function changeDir(dir: DirectoryInfo) {
+        setCurrentDirSeq(dir.dirSeq)
+    }
+
+    //.. 폴더 눌렀을 때
+    function gotoParent() {
+        console.log("gotoParent")
+        getParentDir({
+            ownerId: "김규영",
+            parentSeq: currentDirSeq,
+        }).then(res => {
+            console.log(res.dirSeq);
+            setCurrentDirSeq(res.parentSeq)
+        })
+    }
 
     return (
         <div className="file-list">
-            {dirs.map((dir) => (
-                <BorderLayout key={dir.dirName + dir.dirSeq}>
-                    {dir.dirName}--directory
+            <a onClick={() => gotoParent()}>
+                <BorderLayout>
+                    ..
                 </BorderLayout>
+            </a>
+            {dirs.map((dir) => (
+                <a onClick={() => changeDir(dir)} key={dir.dirSeq + dir.dirName}>
+                    <BorderLayout key={dir.dirName + dir.dirSeq}>
+                        {dir.dirName}--directory
+                    </BorderLayout>
+                </a>
             ))}
             {files.map((file) => (
                 <BorderLayout key={file.storageKey}>
