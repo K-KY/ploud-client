c926c3dd68d2b5e83cce5756c1f7b3954690bfe0
K-KY <kyuyoungk@naver.com>
2025-12-13 16:34:52 +0900

    feat: 디렉토리 모양 아이콘

---
+++ src/components/DirIcon.tsx
@@ -0,0 +1,27 @@
+interface DirIconPros {
+    children: React.ReactNode;
+}
+
+const DirIcon = ({children}: DirIconPros) => {
+    return (
+        <div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <path d="M3 7h5l2 3h11v9a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2z"/>
+            </svg>
+            {children}
+        </div>
+
+    )
+}
+
+export {DirIcon}
