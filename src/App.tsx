b2f8f9d26088a13fde1a420d66e2d12777be89bb
K-KY <kyuyoungk@naver.com>
2026-02-09 17:07:20 +0900

    feat: fileViewer 동작에 따라 현재 위치한 경로를 저장

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import FileUploader from "./components/FileUploader.tsx";
 import {FileViewer} from "./components/FileViewer.tsx";
+import {useState} from "react";
+import type {DirectoryInfo} from "./types/DirectoryInfo.ts";
 
 function App() {
 
+    const [currentDirStack, setCurrentDirStack] = useState<DirectoryInfo[]>([])
     return (
         <>
+            <div>
+                <h3>{currentDirStack.map((c)=> {
+                    return (<div>{c.dirName}</div>)
+                })}</h3>
+            </div>
             <FileUploader></FileUploader>
-            <FileViewer></FileViewer>
+            <FileViewer currentDirStack={currentDirStack} onDirChange={setCurrentDirStack}></FileViewer>
         </>
     )
 }
