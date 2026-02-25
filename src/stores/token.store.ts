db3d46576247fc2918d64549549215783bd58959
K-KY <kyuyoungk@naver.com>
2026-02-25 23:30:00 +0900

    feat: 상태관리 스토리지

---
+++ src/stores/token.store.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand"
+
+interface ATStore {
+    accessToken: string|null;
+    isAuthenticated: boolean
+
+    setAccessToken: (token: string | null) => void;
+    logout: () => void
+}
+
+export const userAuthStore = create<ATStore>((set) => ({
+    accessToken: null,
+    isAuthenticated: false,
+
+    setAccessToken: (token) =>
+        set({
+            accessToken: token,
+            isAuthenticated: !!token,
+        }),
+
+    logout: () =>
+        set({
+            accessToken: null,
+            isAuthenticated: false,
+        }),
+}))
+
