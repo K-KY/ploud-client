70eb866d7e06d0c0e6af0a7a5e0b7b46c8bd9e28
K-KY <kyuyoungk@naver.com>
2026-02-25 23:29:31 +0900

    feat: 로그인 api axios 객체로 호출하도록 변경

--- src/axios/UserApi.ts
+++ src/axios/UserApi.ts
@@ -1,16 +1,33 @@
 import axios from "axios";
 import type {LoginForm} from "../types/LoginForm.ts";
+import {userAuthStore} from "../stores/token.store.ts";
 
-const api = "http://localhost:8080"
+export const api = axios.create({
+
+    baseURL: "http://localhost:8080",
+    withCredentials: true,
+})
 
 const login = async (request: LoginForm) => {
-    return await axios({
-        method: "POST",
-        url: api+"/login",
-        data: request,
-    }).then((res) => {
-        console.log(res);
-    });
+    return await api.post("/login", request)
+        .then((res) => {
+            userAuthStore.setState({
+                accessToken: res.data.accessToken,
+            })
+
+            const state = userAuthStore.getState();
+            console.log(state)
+        });
 }
 
-export {login}
+const refresh = async () => {
+    return await api.post("/refresh")
+        .then((res) => {
+            console.log('refresh success', res.data);
+            userAuthStore.setState({
+                accessToken: res.data.accessToken,
+            })
+        });
+}
+
+export {login, refresh}
