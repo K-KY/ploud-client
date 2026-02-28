ab544a7c2540b03ff42aca8b1d286ac95e4e17fa
K-KY <kyuyoungk@naver.com>
2026-03-01 06:36:27 +0900

    feat: 메타데이터 api 호출 시 jwt를 보내도록 변경

--- src/axios/MetadataApi.ts
+++ src/axios/MetadataApi.ts
@@ -1,16 +1,34 @@
 import axios from "axios";
 import type {StorageInfo} from "../types/StorageInfo.ts";
+import {userAuthStore} from "../stores/token.store.ts";
+import {refresh} from "./UserApi.ts";
 
-const postFile = async (request: StorageInfo, isHls:boolean) => {
-    request.isHls = isHls;
-    console.log(request);
-    return await axios({
-        url:"http://localhost:8080/api/v1/files/upload",
-        method: "POST",
-        data: request,
-    }).then((response) => {
+const api = axios.create({
+    baseURL: "http://localhost:8080",
+    withCredentials: true
+})
+const postFile = async (request: StorageInfo) => {
+    console.log(request)
+    return await api.post("/api/v1/files/upload", request)
+        .then(response => {
         return response.data;
     })
 }
+api.interceptors.request.use(async (config) => {
+    const token = userAuthStore.getState().accessToken
+
+    console.log(token)
+    if (!token) {
+        console.log("리프레시")
+        await refresh()
+        config.headers.Authorization = `Bearer ${userAuthStore.getState().accessToken}`
+
+    }
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+})
 
 export {postFile}
