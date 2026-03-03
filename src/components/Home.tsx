95f9ccd03163032002a91d345dda1adf6183ac1f
K-KY <kyuyoungk@naver.com>
2026-03-03 23:55:31 +0900

    refactor: Home 컴포넌트에서 로그인 여부 확인 함수 제거

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,16 +2,9 @@ import FileUploader from "./FileUploader.tsx";
 import {FileViewer} from "./FileViewer.tsx";
 import {useState} from "react";
 import type {DirectoryInfo} from "../types/DirectoryInfo.ts";
-import {userAuthStore} from "../stores/token.store.ts";
-import {Navigate} from "react-router-dom";
 
 export function Home() {
     const [currentDirStack, setCurrentDirStack] = useState<DirectoryInfo[]>([])
-    if(!userAuthStore.getState().accessToken) {
-        return <Navigate to="/login" replace />;
-    }
-
-
     return (
         <>
             <FileUploader></FileUploader>
