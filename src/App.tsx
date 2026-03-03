74ba6f77ac81a54d5b893ec7a1b2dd93ee41490d
K-KY <kyuyoungk@naver.com>
2026-03-03 23:56:11 +0900

    feat: 라우터에서 Private child로 Home 컴포넌트 넣어서 로그인 여부 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import LoginPage from "./components/LoginPage.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./components/Home.tsx";
+import {Private} from "./components/Private.tsx";
 
 function App() {
 
@@ -9,7 +10,11 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<LoginPage />} />
-                    <Route path="/" element={<Home/>}></Route>
+                    <Route path="/" element={
+                        <Private>
+                            <Home/>
+                        </Private>
+                    }></Route>
                 </Routes>
             </BrowserRouter>
         </>
