86f017c6b76d4055da2060db324d3858bf40e735
K-KY <kyuyoungk@naver.com>
2026-03-04 02:30:57 +0900

    feat: 임시 로그아웃 버튼 App 컴포넌트에 배치

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import LoginPage from "./components/LoginPage.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./components/Home.tsx";
 import {Private} from "./components/Private.tsx";
+import {useEffect, useState} from "react";
+import {logout, refresh} from "./axios/UserApi.ts";
+import {userAuthStore} from "./stores/token.store.ts";
 
 function App() {
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const initAuth = async () => {
+            await refresh();
+            setLoading(false);
+        };
+
+        initAuth();
+    }, []);
+
+    function handleLogout() {
+        console.log("logout")
+        logout()
+        userAuthStore.getState().logout()
+    }
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <>
+            <button onClick={() => handleLogout()}>logout</button>
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<LoginPage />} />
