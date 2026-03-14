5fccecfb93432b1646c880691947c48c74635fa7
K-KY <kyuyoungk@naver.com>
2026-03-14 22:55:49 +0900

    feat: 홈 화면에 파일 업로드 버튼 로그아웃 버튼 추가

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,32 @@
-import FileUploader from "./FileUploader.tsx";
 import {FileViewer} from "./FileViewer.tsx";
 import {useState} from "react";
 import type {DirectoryInfo} from "../types/DirectoryInfo.ts";
+import {Button} from "./Button.tsx";
+import {LinearLayout} from "./LinearLayout.tsx";
+import {useNavigate} from "react-router-dom";
+import {logout} from "../axios/UserApi.ts";
+import {userAuthStore} from "../stores/token.store.ts";
 
 export function Home() {
     const [currentDirStack, setCurrentDirStack] = useState<DirectoryInfo[]>([])
+    const navigate = useNavigate();
+
+    function fileUpload() {
+        navigate("upload")
+    }
+
+    function handleLogout() {
+        console.log("logout")
+        logout()
+        userAuthStore.getState().logout()
+    }
+
     return (
         <>
-            <FileUploader></FileUploader>
+            <LinearLayout>
+                <Button onClick={fileUpload} content={"파일 업로드"}/>
+                <Button onClick={handleLogout} content={"로그아웃"}/>
+            </LinearLayout>
             <FileViewer currentDirStack={currentDirStack} onDirChange={setCurrentDirStack}></FileViewer>
         </>
     )
